694afc438764f722bbd8487795be40b9db9b5247
fet1sov <august9222@gmail.com>
2025-03-12 00:41:45 +0300

    feat(catalog): describe category controller (#2)
    decribe controller for category information fetching
    decribe service for category for iteraction with database through prismaORM
    
    Refs: #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { ClientController } from './client/client.controller';
 import { ClientService } from './client/client.service';
 import { ProductController } from './product/product.controller';
 import { ProductService } from './product/product.service';
+import { CategoryController } from './category/category.controller';
+import { CategoryService } from './category/category.service';
 
 @Module({
   imports: [AuthModule, PrismaModule],
-  controllers: [UserController, ClientController, ProductController],
-  providers: [UserService, ClientService, ProductService],
+  controllers: [UserController, ClientController, ProductController, CategoryController],
+  providers: [UserService, ClientService, ProductService, CategoryService],
 })
 export class AppModule {}
